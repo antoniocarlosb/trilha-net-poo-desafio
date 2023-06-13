6a3b1246f6232b18aa12b7456ea5d35f4fabafd2
antoniocarlosb <104856365+antoniocarlosb@users.noreply.github.com>
2023-06-12 21:50:27 -0300

    Add files via upload

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,19 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero, string modelo, string imei, int memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {      
+            Console.WriteLine($"Instalando aplicativo {nomeApp} no Iphone.");      
+        }        
     }
 }
--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,13 +3,9 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
-        {
-            Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
-        }
+        protected string Modelo { get; set; }
+        protected string Imei { get; set; }
+        protected int Memoria { get; set; } 
 
         public void Ligar()
         {
