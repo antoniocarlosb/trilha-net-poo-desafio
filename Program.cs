2d43191f3f9c9bd0203a45c5450c0247192f2deb
antoniocarlosb <104856365+antoniocarlosb@users.noreply.github.com>
2023-06-12 21:45:55 -0300

    Add files via upload
    Desafio Projeto POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphopne Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 2545", imei: "99999999999", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Watsapp");
+
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphopne IPhone:");
+Smartphone iphone = new Iphone(numero: "123456", modelo: "Modelo 2545", imei: "99999999999", memoria: 64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+
